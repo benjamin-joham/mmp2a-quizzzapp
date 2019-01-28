2503a31ac751bf0ef9568d5b6b26f37857ca338f
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-01-28 20:16:40 +0100

    add impressum.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 //import header from './partials/header'
 import Login from './components/login'
+import Impressum from './components/impressum'
 // import login from './partials/login'
 
 export default class app {
   constructor() {
   //this.login = Login
     this.login = new Login
+    this.impressum = new Impressum
   }
   header() {
     //return this.test.renderLogin()
