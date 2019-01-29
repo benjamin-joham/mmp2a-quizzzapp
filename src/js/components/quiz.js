9b8812bd934aca02a130cf7cc644ccdb33573204
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-01-29 14:33:44 +0100

    Made question number dynamic

--- src/js/components/quiz.js
+++ src/js/components/quiz.js
@@ -1,42 +1,48 @@
 import {h} from 'jsx-dom'
-import * as React from "jsx-dom"
+import * as JSX from "jsx-dom"
 import bem from 'bem-names'
 import api from '../openTriviaApi'
 
 
 const Quiz = () => {
+    let number_of_questions=5;
+    let number_of_player=1;
+    let score=0;
+    let current_question=1
     let question_and_answers
     const api_call= new api
-    const question = React.createRef();
-    const answer1 = React.createRef();
-    const answer2 = React.createRef();
-    const answer3 = React.createRef();
-    const answer4 = React.createRef();
+    const nr = JSX.createRef();
+    const question = JSX.createRef();
+    const answer1 = JSX.createRef();
+    const answer2 = JSX.createRef();
+    const answer3 = JSX.createRef();
+    const answer4 = JSX.createRef();
        
-    api_call.getData(5).then((x)=> {
+    api_call.getData(number_of_questions).then((x)=> {
         question_and_answers=x
         x.map((item)=> {
             console.log(item)
         })
+        nr.current.innerHTML = "Question "+current_question
         question.current.innerHTML = question_and_answers[0].question
 
         let arr = [question_and_answers[0].correct_answer,question_and_answers[0].incorrect_answers[0],question_and_answers[0].incorrect_answers[1],question_and_answers[0].incorrect_answers[2]]
         
-        answer1.current.innerHTML = arr[0]
-        answer2.current.innerHTML = arr[1]
-        answer3.current.innerHTML = arr[2]
-        answer4.current.innerHTML = arr[3]
+        answer1.current.innerHTML ="A: "+ arr[0]  //correct
+        answer2.current.innerHTML ="B: "+ arr[1]
+        answer3.current.innerHTML ="C: "+ arr[2]
+        answer4.current.innerHTML = "D: "+arr[3]
         let rand=Math.floor((Math.random() * 3))
-        answer1.current.innerHTML = arr[rand]
+        answer1.current.innerHTML = "A: "+arr[rand]
         switch(rand) {
             case 1:
-            answer2.current.innerHTML = arr[0]
+            answer2.current.innerHTML ="B: "+ arr[0]  //correct
                 break;
             case 2:
-            answer3.current.innerHTML = arr[0]
+            answer3.current.innerHTML ="C: "+ arr[0]
                 break;
             case 3:
-            answer4.current.innerHTML = arr[0]
+            answer4.current.innerHTML ="D: "+ arr[0]
                 break;
             default:
                 break;
@@ -46,14 +52,14 @@ const Quiz = () => {
     return(
         <section className={bem('quiz')}>
         <article className={bem('question')}>
-            <h2 className={bem('question', 'h2')}>Question 1</h2>
+            <h2 className={bem('question', 'h2')} ref={nr}></h2>
             <p className={bem('question', 'p')} ref={question}></p>
         </article>
         <article className={bem('answers')}>
-            <p className={bem('answers','p')}><button className={bem('answers','button')} ref={answer1}></button>
-            <button className={bem('answers','button')} ref={answer2}></button></p>
-            <p className={bem('answers','p')}><button className={bem('answers','button')} ref={answer3}></button>
-            <button className={bem('answers','button')} ref={answer4}></button></p>
+            <p className={bem('answers','p')}><button className={bem('answers','button')} active='false' ref={answer1}></button>
+            <button className={bem('answers','button')} active='false' ref={answer2}></button></p>
+            <p className={bem('answers','p')}><button className={bem('answers','button')} active='false' ref={answer3}></button>
+            <button className={bem('answers','button')} active='false' ref={answer4}></button></p>
         </article>
         </section>
     )
