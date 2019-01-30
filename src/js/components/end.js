42be87c711091cd7354688400d391943e74e34cb
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-01-30 17:41:42 +0100

    update

--- src/js/components/end.js
+++ src/js/components/end.js
@@ -23,9 +23,9 @@ if(number_of_players==1){   //singleplayer
         button2_text='Play again!'
     }
 } else {    //multiplayer
-    for(let i = 1; i<= number_of_players; i++)
+    for(let i = 0; i< number_of_players; i++)
     {
-        result+='Player '+i+' answered '+correct_questions+'/'+questions_total+' Questions correct. '
+        result+='Player '+number_of_players+' answered '+correct_questions+'/'+questions_total+' Questions correct.'
     }
     if (user) {
         button1_text='View statistic!'
