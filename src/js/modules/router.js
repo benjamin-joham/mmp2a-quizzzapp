49cf06a49b7ff41c97e67eced6a210a7587c754f
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-01-30 10:43:26 +0100

    router refactored

---
+++ src/js/modules/router.js
@@ -0,0 +1,40 @@
+import Navigo from 'navigo'
+
+
+
+import { h } from 'jsx-dom'
+import LoginScreen from '../partials/loginScreen';
+import StartScreen from '../partials/startScreen';
+import QuizzScreen from '../partials/quizzScreen';
+const router = new Navigo(window.location.origin)
+
+const body = document.querySelector('body')
+
+router.notFound(() => {
+  body.appendChild(<h1>404 Page not found</h1>)
+  console.log('404')
+})
+
+router
+  .on(
+    {
+      '/': () => {
+        console.log('jetzt in root')
+        // append LoginScreen
+        body.innerHTML = ''
+        body.appendChild(<LoginScreen/>)
+      },
+      'start': () => {
+      // append StartScreen
+      body.innerHTML = ''
+      body.appendChild(<StartScreen />)
+      },
+      'quiz/:id': (params) => {
+        body.innerHTML = ''
+        body.appendChild(<QuizzScreen question={params.id}/>)
+      },
+      'test': () => {
+      }
+    });
+
+  export default router;
