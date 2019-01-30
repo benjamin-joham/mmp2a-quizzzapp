0412e3a77280c8b3873c45bcca4d3739c08aba78
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-01-30 10:44:17 +0100

    Try statistic

--- src/js/components/profile.js
+++ src/js/components/profile.js
@@ -2,9 +2,37 @@ import { h } from 'jsx-dom'
 import bem from 'bem-names'
 
 const Profile = () => {
-
-<h1>hallo!!!</h1>
-
+  let wrong_questions_last=1
+  let correct_questions_last=4
+  let ctx = document.getElementById("myChart").getContext('2d');
+  let myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Wrong", "Correct"],
+          datasets: [{
+              label: '# of Questions',
+              data: [wrong_questions_last, correct_questions_last],
+              backgroundColor: [
+                  'rgba(210, 0, 25, 0.2)',
+                  'rgba(0, 190, 25, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,255,255,1)',
+                  'rgba(255, 255, 255, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
 
 
   return (
