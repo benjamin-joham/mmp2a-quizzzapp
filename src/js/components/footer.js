1793f402fa32ea62a1aec720977a6aaa069e776f
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-02-01 10:07:32 +0100

    add datenschutzerklärung

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -2,21 +2,29 @@ import { h } from 'jsx-dom' // eslint-disable-line no-use-before-define
 import bem from 'bem-names'
 import * as React from 'jsx-dom'
 import Impressum from '../components/impressum';
+import Privacy from '../components/privacy';
 
 const Footer = () => {
-    const impr = React.createRef()
+    const f = React.createRef()
     
     const clickImpressum = (event) => {
-            impr.current.appendChild(<Impressum/>)
-            let link = document.getElementById('footer_link')
+            f.current.appendChild(<Impressum/>)
+            let link = document.getElementById('footer_link_impr')
             link.style.visibility = 'hidden'
     }
 
+    const clickPrivacy = (event) => {
+      f.current.appendChild(<Privacy/>)
+      let link = document.getElementById('footer_link_priv')
+      link.style.visibility = 'hidden'
+}
+
 
   return (
-      <div className={bem('div')} ref={impr}>
+      <div className={bem('div')} ref={f}>
       <footer className={bem('footer')}>
-        <a id='footer_link' className={bem('footer', 'a')} href='#impressum' onClick={ clickImpressum } >About</a>
+        <a id='footer_link_ipmr' className={bem('footer', 'a')} href='#impressum' onClick={ clickImpressum } >About</a>
+        <a id='footer_link_priv' className={bem('footer', 'a')} href='#privacy' onClick={ clickPrivacy} >Privacy</a>
     </footer>
     </div>
   )
