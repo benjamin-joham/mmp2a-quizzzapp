a2d86ff1074e841384e6bb4b063da87a4a24a60c
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-03-19 16:15:34 +0100

    Remove some console.logs from startScreen.js

--- src/js/partials/startScreen.js
+++ src/js/partials/startScreen.js
@@ -7,32 +7,18 @@ import router from '../modules/router'
 
 const clickHandler = async (event) => {
   event.preventDefault()
-  console.log(event.target)
   let players = document.getElementById('btn_players_active')
   let questions = document.getElementById('btn_questions_active')
 
-  console.log(players)
-  console.log(questions)
-  console.log('amount of players: ', players.value)
-  console.log('amount of questions: ', questions.value)
-
-  console.log(api(questions.value)
+  console.log(api(questions.value)  //wtf?!?
     .then(x => {
       window.questions = x
-      // localStorage.setItem('questions', JSON.stringify(x))
-      console.log(x)
     })
     .then(() => {
-      // localStorage.removeItem('scores')
       localStorage.clear()
-      console.log('scores: ', JSON.parse(localStorage.getItem('scores')))
       if (players.value > 1) { router.navigate('/quiz?mulitplayer=true&amountPlayer=' + players.value + '&question=1&player=1') } else { router.navigate('/quiz?mulitplayer=false&amountPlayer=' + players.value + '&question=1&player=1') }
     })
   )
-
-  // window.questions = await api(questions.value).then(x => x)
-
-  // localStorage.setItem('questions', JSON.stringify(window.questions))
 }
 
 const StartScreen = () => {
