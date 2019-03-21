f11ff8679d5070892f51d948b418c3271ae415d0
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-03-21 15:46:35 +0100

    Delete console.logs

--- src/js/components/end.js
+++ src/js/components/end.js
@@ -5,19 +5,22 @@ import { GetAllUsers, AddNewQuestionsetToFirestore, updateFirestore } from '../m
 import router from '../modules/router'
 
 const End = ({ children, ...props }) => {
-  let correctQuestions = JSON.parse(localStorage.getItem('scores'))
-  let questionsTotal
+  let correct_questions = JSON.parse(localStorage.getItem('scores'))
+  let questions_total
   if (window.challenge) {
-    questionsTotal = Object.values(window.questions).length
+    questions_total = Object.values(window.questions).length
   } else {
-    questionsTotal = window.questions.length
+    questions_total = window.questions.length
   }
-  let numberOfPlayers = correctQuestions.length
-  let button1Text = 'View statistic!'
-  let button2Text = 'Play again!'
-  let button3Text = 'Challenge a friend!'
+  let number_of_players = correct_questions.length
+  let button1_text = 'View statistic!'
+  let button2_text = 'Play again!'
+  let button3_text = 'Challenge a friend!'
   let result = ''
-  let loggedIn = window.user
+  let logged_in = window.user
+
+  // const resultContainer = document.querySelector('.end__div')
+  // console.log("BAGUETTE",window.questions)
 
   let checkIfNumber = (number) => {
     if (number != null) { return number } else { return 0 }
@@ -25,9 +28,9 @@ const End = ({ children, ...props }) => {
 
   let getContent = () => {
     let add
-    if (numberOfPlayers == 1) { // singleplayer
-      if (loggedIn) { add = addButtons() }
-      result = 'You answered ' + checkIfNumber(correctQuestions[0]) + '/' + questionsTotal + ' Questions correct.'
+    if (number_of_players == 1) { // singleplayer
+      if (logged_in) { add = addButtons() }
+      result = 'You answered ' + checkIfNumber(correct_questions[0]) + '/' + questions_total + ' Questions correct.'
       return (
         <div className={bem('end', 'div')}>
           <h2 className={bem('end', 'h2')}>{result}</h2>
@@ -36,36 +39,47 @@ const End = ({ children, ...props }) => {
       )
     } else { // multiplayer
       let content = <div className={bem('end', 'div')}></div>
-      for (let i = 1; i <= numberOfPlayers; i++) {
-        result = 'Player ' + i + ' answered ' + checkIfNumber(correctQuestions[i - 1]) + '/' + questionsTotal + ' Questions correct.'
+      for (let i = 1; i <= number_of_players; i++) {
+        result = 'Player ' + i + ' answered ' + checkIfNumber(correct_questions[i - 1]) + '/' + questions_total + ' Questions correct.'
         content.appendChild(<h2 className={bem('end', 'h2')}>{result}</h2>)
       }
       return content
     }
   }
-
+  // q_a = {
+  //   0: {
+  //     q: string,
+  //     corr: string,
+  //     wrong: [a,b,c],
+  //   }
+  // }
   let sendChallenge = async (challenger) => {
     let questionarr = []
     let answers = {}
-    let answerArr
+    let answerarr
     let data = {}
 
-    for (let i = 0; i < questionsTotal; i++) {
-      answerArr = []
+    for (let i = 0; i < questions_total; i++) {
+      answerarr = []
       questionarr.push(window.questions[i].question)
-      answerArr.push(window.questions[i].correctAnswer)
-      window.questions[i].incorrectAnswers.forEach(wrong => {
-        answerArr.push(wrong)
+      answerarr.push(window.questions[i].correct_answer)
+      window.questions[i].incorrect_answers.forEach(wrong => {
+        answerarr.push(wrong)
       })
-      answers.correct = answerArr[0]
-      answers.wrong = [answerArr[1], answerArr[2], answerArr[3]]
+      // console.log(answerarr)
+      answers.correct = answerarr[0]
+      answers.wrong = [answerarr[1], answerarr[2], answerarr[3]]
       data[i] = {
         question: window.questions[i].question,
-        correctAnswer: answerArr[0],
-        incorrectAnswers: [answerArr[1], answerArr[2], answerArr[3]]
+        correct_answer: answerarr[0],
+        incorrect_answers: [answerarr[1], answerarr[2], answerarr[3]]
       }
     }
-
+    // console.log('q&a', data)
+    // console.log(Object.values(data).length)
+    // console.log(questionarr)
+    // console.log(answerarr)
+    // console.log(answers)
     let points = JSON.parse(localStorage.getItem('scores'))
 
     let response = await AddNewQuestionsetToFirestore(data, window.user.name, challenger, points[0])
@@ -81,18 +95,17 @@ const End = ({ children, ...props }) => {
 
   const showChallenger = async () => {
     let users = await GetAllUsers()
+    // console.log(users)
+    // console.log(users.then(i => console.log(i)))
     let container = document.querySelector('section.end')
-    let select = <select className={bem('end', 'select')} id='challenger' name="challenger" size="5" onClick={(e) => console.log(e.target)}></select>
-
-    users.forEach(i => {
-      if (i != window.user.name) {
-        select.appendChild(<option className={bem('select', 'option')} value={i}>{i}</option>)
-      }
-    })
+    let select = <select id='challenger' name="challenger" size="5" onClick={(e) => console.log(e.target)}></select>
+    { users.forEach(i => {
+      if (i != window.user.name) { select.appendChild(<option value={i}>{i}</option>) }
+    }) }
     let content = container.appendChild(
       <div className={bem('end', 'div', ['challenge'])}>
-        <h1 className={bem()}>Choose your Challenger</h1>
-        <form className={bem('end', 'form')}>
+        <h1>Choose your Challenger</h1>
+        <form>
           {select}
           <button onClick={
             (e) => {
@@ -112,17 +125,17 @@ const End = ({ children, ...props }) => {
   }
 
   let addButtons = () => {
-    if (loggedIn) {
+    if (logged_in) {
       return (
         <div>
           <button className={bem('button')} onClick={() => {
             updateFirestore()
             router.navigate('/profile')
           }}>
-            {button1Text}
+            {button1_text}
           </button>
           <button className={bem('button')} onClick={() => showChallenger()}>
-            {button3Text}
+            {button3_text}
           </button>
         </div>
       )
@@ -139,7 +152,7 @@ const End = ({ children, ...props }) => {
           router.navigate('/start')
         }
         }>
-        {button2Text}
+        {button2_text}
       </button>
     </section>
   )
