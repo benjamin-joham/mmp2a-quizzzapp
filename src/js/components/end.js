f15e043fad406a325b25aa94777dc6ec20b7e15f
fhs41349 <vmaurer.mmt-b2017@fh-salzburg.ac.at>
2019-03-21 15:49:18 +0100

    Change variable names

--- src/js/components/end.js
+++ src/js/components/end.js
@@ -5,22 +5,19 @@ import { GetAllUsers, AddNewQuestionsetToFirestore, updateFirestore } from '../m
 import router from '../modules/router'
 
 const End = ({ children, ...props }) => {
-  let correct_questions = JSON.parse(localStorage.getItem('scores'))
-  let questions_total
+  let correctQuestions = JSON.parse(localStorage.getItem('scores'))
+  let questionsTotal
   if (window.challenge) {
-    questions_total = Object.values(window.questions).length
+    questionsTotal = Object.values(window.questions).length
   } else {
-    questions_total = window.questions.length
+    questionsTotal = window.questions.length
   }
-  let number_of_players = correct_questions.length
-  let button1_text = 'View statistic!'
-  let button2_text = 'Play again!'
-  let button3_text = 'Challenge a friend!'
+  let numberOfPlayers = correctQuestions.length
+  let button1Text = 'View statistic!'
+  let button2Text = 'Play again!'
+  let button3Text = 'Challenge a friend!'
   let result = ''
   let logged_in = window.user
-
-  // const resultContainer = document.querySelector('.end__div')
-  // console.log("BAGUETTE",window.questions)
 
   let checkIfNumber = (number) => {
     if (number != null) { return number } else { return 0 }
@@ -28,9 +25,9 @@ const End = ({ children, ...props }) => {
 
   let getContent = () => {
     let add
-    if (number_of_players == 1) { // singleplayer
+    if (numberOfPlayers == 1) { // singleplayer
       if (logged_in) { add = addButtons() }
-      result = 'You answered ' + checkIfNumber(correct_questions[0]) + '/' + questions_total + ' Questions correct.'
+      result = 'You answered ' + checkIfNumber(correctQuestions[0]) + '/' + questionsTotal + ' Questions correct.'
       return (
         <div className={bem('end', 'div')}>
           <h2 className={bem('end', 'h2')}>{result}</h2>
@@ -39,34 +36,27 @@ const End = ({ children, ...props }) => {
       )
     } else { // multiplayer
       let content = <div className={bem('end', 'div')}></div>
-      for (let i = 1; i <= number_of_players; i++) {
-        result = 'Player ' + i + ' answered ' + checkIfNumber(correct_questions[i - 1]) + '/' + questions_total + ' Questions correct.'
+      for (let i = 1; i <= numberOfPlayers; i++) {
+        result = 'Player ' + i + ' answered ' + checkIfNumber(correctQuestions[i - 1]) + '/' + questionsTotal + ' Questions correct.'
         content.appendChild(<h2 className={bem('end', 'h2')}>{result}</h2>)
       }
       return content
     }
   }
-  // q_a = {
-  //   0: {
-  //     q: string,
-  //     corr: string,
-  //     wrong: [a,b,c],
-  //   }
-  // }
+
   let sendChallenge = async (challenger) => {
     let questionarr = []
     let answers = {}
     let answerarr
     let data = {}
 
-    for (let i = 0; i < questions_total; i++) {
+    for (let i = 0; i < questionsTotal; i++) {
       answerarr = []
       questionarr.push(window.questions[i].question)
       answerarr.push(window.questions[i].correct_answer)
       window.questions[i].incorrect_answers.forEach(wrong => {
         answerarr.push(wrong)
       })
-      // console.log(answerarr)
       answers.correct = answerarr[0]
       answers.wrong = [answerarr[1], answerarr[2], answerarr[3]]
       data[i] = {
@@ -75,11 +65,7 @@ const End = ({ children, ...props }) => {
         incorrect_answers: [answerarr[1], answerarr[2], answerarr[3]]
       }
     }
-    // console.log('q&a', data)
-    // console.log(Object.values(data).length)
-    // console.log(questionarr)
-    // console.log(answerarr)
-    // console.log(answers)
+
     let points = JSON.parse(localStorage.getItem('scores'))
 
     let response = await AddNewQuestionsetToFirestore(data, window.user.name, challenger, points[0])
@@ -95,8 +81,7 @@ const End = ({ children, ...props }) => {
 
   const showChallenger = async () => {
     let users = await GetAllUsers()
-    // console.log(users)
-    // console.log(users.then(i => console.log(i)))
+
     let container = document.querySelector('section.end')
     let select = <select id='challenger' name="challenger" size="5" onClick={(e) => console.log(e.target)}></select>
     { users.forEach(i => {
@@ -132,10 +117,10 @@ const End = ({ children, ...props }) => {
             updateFirestore()
             router.navigate('/profile')
           }}>
-            {button1_text}
+            {button1Text}
           </button>
           <button className={bem('button')} onClick={() => showChallenger()}>
-            {button3_text}
+            {button3Text}
           </button>
         </div>
       )
@@ -152,7 +137,7 @@ const End = ({ children, ...props }) => {
           router.navigate('/start')
         }
         }>
-        {button2_text}
+        {button2Text}
       </button>
     </section>
   )
